7c61d9bc954373dc9db4e5136500ec9df6a7b964
le-garden-fox <le-garden-fox@example.com>
2020-09-13 20:02:59 -0700

    Update the cmd.sh script

--- {{cookiecutter.project_name}}/cmd.sh
+++ {{cookiecutter.project_name}}/cmd.sh
@@ -2,30 +2,17 @@
 
 function main() {
     case $1 in
-    "build-grpc")
-        docker build -t edgar/gogrpc -f Dockerfile.grpc .
-        ;;
     "build-server")
-        docker build -t edgar/pbserver -f Dockerfile.server .
-        ;;
-    "build-client")
-        docker build -t edgar/pbclient -f Dockerfile.client .
+        docker build -t edgar/{{cookiecutter.module_name}} -f Dockerfile.server .
         ;;
     "run-server")
-        docker run -it edgar/pbserver -p 50051:50051
-        ;;
-    "run-client")
-        echo "TODO"
-        #docker run -it edgar/pbserver -p 50051:50051
+        docker run -it edgar/{{cookiecutter.module_name}} -p {{cookiecutter.server_port}}:{{cookiecutter.server_port}}
         ;;
     "gen")
         protoc {{cookiecutter.module_name}}/{{cookiecutter.module_name}}pb/{{cookiecutter.module_name}}.proto --go_out=plugins=grpc:.
         ;;
     "tst-server")
         go run {{cookiecutter.module_name}}/{{cookiecutter.module_name}}-server/server.go
-        ;;
-    "tst-client")
-        go run {{cookiecutter.module_name}}/{{cookiecutter.module_name}}-client/client.go
         ;;
     *)
         echo "Invalid arguments"
